27dc585ff0b7d392266be1b97c691cb42caf79a1
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-09-20 01:17:26 +0700

    first commit

---
+++ Notebooks/Cleaning.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load dataset and describe the data
+retail = pd.read_csv("Dataset\online_retail.csv",on_bad_lines='skip', sep=";", decimal= ",", encoding="latin1"  )
+retail.info()
+print(retail.shape)    # lihat jumlah baris & kolom
+print(retail.columns)  # lihat nama kolom
+print(retail.dtypes)   # tipe data tiap kolom
+print(retail.head())   # lihat 5 baris pertama
+
+#'InvoiceNo', 'StockCode', 'Description', 'Quantity', 'InvoiceDate', 'UnitPrice', 'CustomerID', 'Country'
+
+# =============== Cleaning Data ===============
+# Ubah tipe data
+retail['InvoiceDate'] = pd.to_datetime(retail['InvoiceDate'], dayfirst=True)
+# Buang nilai Quantity negatif
+retail = retail[retail['Quantity'] > 0] 
+# Hapus baris dengan CustomerID kosong
+retail = retail.dropna(subset=['CustomerID'])
+# Hapus harga yang tidak valid
+retail = retail[retail['UnitPrice'] > 0]
+# Reset index
+retail = retail.reset_index(drop=True)
+# Cek hasil awal
+print(retail.info())
+print(retail.describe())
+
