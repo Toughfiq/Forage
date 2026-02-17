0b66cf4ffeda5c38c5168e3d1f48ad6a4e953f9b
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2026-02-17 20:53:09 +0700

    membenarkan sebuah line baru

--- Notebooks/Analysis.py
+++ Notebooks/Analysis.py
@@ -6,3 +6,11 @@ import seaborn as sns
 rfm = pd.read_csv("Dataset\online_retail_cleaned.csv")
 rfm.info()
 print(rfm.describe())
+rfm['InvoiceDate'] = pd.to_datetime(rfm['InvoiceDate'])
+reference_date = rfm['InvoiceDate'].max()
+rfm = rfm.groupby('CustomerID').agg(
+    last_purchase = ('InvoiceDate', 'max'),
+    Frequency     = ('InvoiceNo', 'nunique'),
+    Monetary      = ('Revenue', 'sum')  
+)
+rfm['Recency'] = (reference_date - rfm['last_purchase']).dt.days  
