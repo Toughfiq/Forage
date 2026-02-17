1e96a3897e035862e60a7c7d63507fa3b027c3b9
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2026-02-17 23:26:57 +0700

    Menambah kmeans anaylisis dengan 3 cluster

--- Notebooks/Analysis.py
+++ Notebooks/Analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
 # Load dataset
 rfm = pd.read_csv("Dataset\online_retail_cleaned.csv")
 rfm.info()
@@ -20,4 +21,27 @@ rfm['Recency'] = (reference_date - rfm['last_purchase']).dt.days
 # Scale data
 rfm_features = rfm[['Recency', 'Frequency', 'Monetary']]
 scaler = StandardScaler()
-rfm_scaled = scaler.fit_transform(rfm_features)
+rfm_scaled = scaler.fit_transform(rfm_features) # Standarisasi data RFM
+# tidak perlu menggunakan kutip karena variabel rfm_scaled sudah berupa array numpy.
+
+#elbow plot untuk menentukan jumlah cluster optimal
+
+# Elbow Plot
+wcss = []
+for k in range(1, 11):
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(rfm_scaled)
+    wcss.append(kmeans.inertia_)
+
+plt.figure(figsize=(8, 5))
+plt.plot(range(1, 11), wcss, marker='o')
+plt.title('Elbow Method')
+plt.xlabel('Cluster')
+plt.ylabel('WCSS Value')
+plt.show()
+
+# K-Means Clustering
+kmeans = KMeans(n_clusters=3, random_state=42)  
+kmeans.fit(rfm_scaled)                          
+rfm['cluster_final'] = kmeans.labels_
+print(rfm.groupby('cluster_final')[['Recency', 'Frequency', 'Monetary']].mean())
