61fd424452f6b92aba847254ece884b0707c4160
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2026-02-17 22:38:22 +0700

    Menambah scaler untuk data RFM

--- Notebooks/Analysis.py
+++ Notebooks/Analysis.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from sklearn.preprocessing import StandardScaler
 # Load dataset
 rfm = pd.read_csv("Dataset\online_retail_cleaned.csv")
 rfm.info()
 print(rfm.describe())
 rfm['InvoiceDate'] = pd.to_datetime(rfm['InvoiceDate'])
+# Diperlukan merubah kembali ke datetime karena sebelumnya sudah diubah ke string 
+# saat menyimpan ke CSV.
 reference_date = rfm['InvoiceDate'].max()
 rfm = rfm.groupby('CustomerID').agg(
     last_purchase = ('InvoiceDate', 'max'),
@@ -14,3 +16,8 @@ rfm = rfm.groupby('CustomerID').agg(
     Monetary      = ('Revenue', 'sum')  
 )
 rfm['Recency'] = (reference_date - rfm['last_purchase']).dt.days  
+
+# Scale data
+rfm_features = rfm[['Recency', 'Frequency', 'Monetary']]
+scaler = StandardScaler()
+rfm_scaled = scaler.fit_transform(rfm_features)
